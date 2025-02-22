640e22dbabafb7641a95af54ceb531ffd0d8908c
CASTEGNARO EROS <10933991@itisrossi.edu.it>
2025-02-22 09:49:16 +0100

    iniziale

---
+++ prova3/prova3/Form1.Designer.cs
@@ -0,0 +1,279 @@
+﻿namespace prova3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Pulire le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione Windows Form
+
+        /// <summary>
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn1 = new System.Windows.Forms.Button();
+            this.lbl_testo = new System.Windows.Forms.Label();
+            this.btn4 = new System.Windows.Forms.Button();
+            this.btn7 = new System.Windows.Forms.Button();
+            this.btn8 = new System.Windows.Forms.Button();
+            this.btn5 = new System.Windows.Forms.Button();
+            this.btn2 = new System.Windows.Forms.Button();
+            this.btn9 = new System.Windows.Forms.Button();
+            this.btn6 = new System.Windows.Forms.Button();
+            this.btn3 = new System.Windows.Forms.Button();
+            this.btnMoltiplicazione = new System.Windows.Forms.Button();
+            this.btnSottrazione = new System.Windows.Forms.Button();
+            this.btnSomma = new System.Windows.Forms.Button();
+            this.btnDivisione = new System.Windows.Forms.Button();
+            this.btnUguale = new System.Windows.Forms.Button();
+            this.btn0 = new System.Windows.Forms.Button();
+            this.btnCancella = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btn1
+            // 
+            this.btn1.Location = new System.Drawing.Point(67, 295);
+            this.btn1.Name = "btn1";
+            this.btn1.Size = new System.Drawing.Size(60, 60);
+            this.btn1.TabIndex = 0;
+            this.btn1.Text = "1";
+            this.btn1.UseVisualStyleBackColor = true;
+            // 
+            // lbl_testo
+            // 
+            this.lbl_testo.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.lbl_testo.Location = new System.Drawing.Point(70, 116);
+            this.lbl_testo.Name = "lbl_testo";
+            this.lbl_testo.Size = new System.Drawing.Size(349, 30);
+            this.lbl_testo.TabIndex = 1;
+            this.lbl_testo.Text = "Calcolo";
+            // 
+            // btn4
+            // 
+            this.btn4.Location = new System.Drawing.Point(67, 229);
+            this.btn4.Name = "btn4";
+            this.btn4.Size = new System.Drawing.Size(60, 60);
+            this.btn4.TabIndex = 4;
+            this.btn4.Text = "4";
+            this.btn4.UseVisualStyleBackColor = true;
+            this.btn4.Click += new System.EventHandler(this.btn4_Click);
+            // 
+            // btn7
+            // 
+            this.btn7.Location = new System.Drawing.Point(67, 163);
+            this.btn7.Name = "btn7";
+            this.btn7.Size = new System.Drawing.Size(60, 60);
+            this.btn7.TabIndex = 7;
+            this.btn7.Text = "7";
+            this.btn7.UseVisualStyleBackColor = true;
+            this.btn7.Click += new System.EventHandler(this.btn7_Click);
+            // 
+            // btn8
+            // 
+            this.btn8.Location = new System.Drawing.Point(133, 163);
+            this.btn8.Name = "btn8";
+            this.btn8.Size = new System.Drawing.Size(60, 60);
+            this.btn8.TabIndex = 8;
+            this.btn8.Text = "8";
+            this.btn8.UseVisualStyleBackColor = true;
+            this.btn8.Click += new System.EventHandler(this.btn8_Click);
+            // 
+            // btn5
+            // 
+            this.btn5.Location = new System.Drawing.Point(133, 229);
+            this.btn5.Name = "btn5";
+            this.btn5.Size = new System.Drawing.Size(60, 60);
+            this.btn5.TabIndex = 5;
+            this.btn5.Text = "5";
+            this.btn5.UseVisualStyleBackColor = true;
+            this.btn5.Click += new System.EventHandler(this.btn5_Click);
+            // 
+            // btn2
+            // 
+            this.btn2.Location = new System.Drawing.Point(133, 295);
+            this.btn2.Name = "btn2";
+            this.btn2.Size = new System.Drawing.Size(60, 60);
+            this.btn2.TabIndex = 2;
+            this.btn2.Text = "2";
+            this.btn2.UseVisualStyleBackColor = true;
+            this.btn2.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // btn9
+            // 
+            this.btn9.Location = new System.Drawing.Point(199, 163);
+            this.btn9.Name = "btn9";
+            this.btn9.Size = new System.Drawing.Size(60, 60);
+            this.btn9.TabIndex = 9;
+            this.btn9.Text = "9";
+            this.btn9.UseVisualStyleBackColor = true;
+            this.btn9.Click += new System.EventHandler(this.btn9_Click);
+            // 
+            // btn6
+            // 
+            this.btn6.Location = new System.Drawing.Point(199, 229);
+            this.btn6.Name = "btn6";
+            this.btn6.Size = new System.Drawing.Size(60, 60);
+            this.btn6.TabIndex = 6;
+            this.btn6.Text = "6";
+            this.btn6.UseVisualStyleBackColor = true;
+            this.btn6.Click += new System.EventHandler(this.btn6_Click);
+            // 
+            // btn3
+            // 
+            this.btn3.Location = new System.Drawing.Point(199, 295);
+            this.btn3.Name = "btn3";
+            this.btn3.Size = new System.Drawing.Size(60, 60);
+            this.btn3.TabIndex = 3;
+            this.btn3.Text = "3";
+            this.btn3.UseVisualStyleBackColor = true;
+            this.btn3.Click += new System.EventHandler(this.btn3_Click);
+            // 
+            // btnMoltiplicazione
+            // 
+            this.btnMoltiplicazione.Location = new System.Drawing.Point(293, 229);
+            this.btnMoltiplicazione.Name = "btnMoltiplicazione";
+            this.btnMoltiplicazione.Size = new System.Drawing.Size(60, 60);
+            this.btnMoltiplicazione.TabIndex = 10;
+            this.btnMoltiplicazione.Text = "x";
+            this.btnMoltiplicazione.UseVisualStyleBackColor = true;
+            this.btnMoltiplicazione.Click += new System.EventHandler(this.btnMoltiplicazione_Click);
+            // 
+            // btnSottrazione
+            // 
+            this.btnSottrazione.Location = new System.Drawing.Point(293, 295);
+            this.btnSottrazione.Name = "btnSottrazione";
+            this.btnSottrazione.Size = new System.Drawing.Size(60, 60);
+            this.btnSottrazione.TabIndex = 11;
+            this.btnSottrazione.Text = "-";
+            this.btnSottrazione.UseVisualStyleBackColor = true;
+            this.btnSottrazione.Click += new System.EventHandler(this.btnSottrazione_Click);
+            // 
+            // btnSomma
+            // 
+            this.btnSomma.Location = new System.Drawing.Point(293, 361);
+            this.btnSomma.Name = "btnSomma";
+            this.btnSomma.Size = new System.Drawing.Size(60, 60);
+            this.btnSomma.TabIndex = 12;
+            this.btnSomma.Text = "+";
+            this.btnSomma.UseVisualStyleBackColor = true;
+            this.btnSomma.Click += new System.EventHandler(this.btnSomma_Click);
+            // 
+            // btnDivisione
+            // 
+            this.btnDivisione.Location = new System.Drawing.Point(359, 229);
+            this.btnDivisione.Name = "btnDivisione";
+            this.btnDivisione.Size = new System.Drawing.Size(60, 60);
+            this.btnDivisione.TabIndex = 13;
+            this.btnDivisione.Text = "÷";
+            this.btnDivisione.UseVisualStyleBackColor = true;
+            this.btnDivisione.Click += new System.EventHandler(this.btnDivisione_Click);
+            // 
+            // btnUguale
+            // 
+            this.btnUguale.Location = new System.Drawing.Point(359, 295);
+            this.btnUguale.Name = "btnUguale";
+            this.btnUguale.Size = new System.Drawing.Size(60, 126);
+            this.btnUguale.TabIndex = 14;
+            this.btnUguale.Text = "=";
+            this.btnUguale.UseVisualStyleBackColor = true;
+            this.btnUguale.Click += new System.EventHandler(this.btnUguale_Click);
+            // 
+            // btn0
+            // 
+            this.btn0.Location = new System.Drawing.Point(67, 361);
+            this.btn0.Name = "btn0";
+            this.btn0.Size = new System.Drawing.Size(192, 60);
+            this.btn0.TabIndex = 15;
+            this.btn0.Text = "0";
+            this.btn0.UseVisualStyleBackColor = true;
+            this.btn0.Click += new System.EventHandler(this.btn0_Click);
+            // 
+            // btnCancella
+            // 
+            this.btnCancella.Location = new System.Drawing.Point(293, 163);
+            this.btnCancella.Name = "btnCancella";
+            this.btnCancella.Size = new System.Drawing.Size(126, 60);
+            this.btnCancella.TabIndex = 16;
+            this.btnCancella.Text = "⌫";
+            this.btnCancella.UseVisualStyleBackColor = true;
+            this.btnCancella.Click += new System.EventHandler(this.btnCancella_Click);
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(67, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(352, 107);
+            this.label1.TabIndex = 17;
+            this.label1.Text = "Storico";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(484, 461);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnCancella);
+            this.Controls.Add(this.btn0);
+            this.Controls.Add(this.btnUguale);
+            this.Controls.Add(this.btnDivisione);
+            this.Controls.Add(this.btnSomma);
+            this.Controls.Add(this.btnSottrazione);
+            this.Controls.Add(this.btnMoltiplicazione);
+            this.Controls.Add(this.btn3);
+            this.Controls.Add(this.btn6);
+            this.Controls.Add(this.btn9);
+            this.Controls.Add(this.btn2);
+            this.Controls.Add(this.btn5);
+            this.Controls.Add(this.btn8);
+            this.Controls.Add(this.btn7);
+            this.Controls.Add(this.btn4);
+            this.Controls.Add(this.lbl_testo);
+            this.Controls.Add(this.btn1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn1;
+        private System.Windows.Forms.Label lbl_testo;
+        private System.Windows.Forms.Button btn4;
+        private System.Windows.Forms.Button btn7;
+        private System.Windows.Forms.Button btn8;
+        private System.Windows.Forms.Button btn5;
+        private System.Windows.Forms.Button btn2;
+        private System.Windows.Forms.Button btn9;
+        private System.Windows.Forms.Button btn6;
+        private System.Windows.Forms.Button btn3;
+        private System.Windows.Forms.Button btnMoltiplicazione;
+        private System.Windows.Forms.Button btnSottrazione;
+        private System.Windows.Forms.Button btnSomma;
+        private System.Windows.Forms.Button btnDivisione;
+        private System.Windows.Forms.Button btnUguale;
+        private System.Windows.Forms.Button btn0;
+        private System.Windows.Forms.Button btnCancella;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ prova3/prova3/Form1.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+namespace prova3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private void lbl_testo_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn9_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn8_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn7_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnCancella_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn0_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnUguale_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnDivisione_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnSomma_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnSottrazione_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnMoltiplicazione_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
